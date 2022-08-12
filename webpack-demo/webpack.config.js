864f687100e8e003e41afc5c149d106edb09d749
tahseenzahra <tahseen1zahra@gmail.com>
2022-08-12 15:03:58 +0500

    Add webpack-dev-server

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -4,6 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -13,6 +16,9 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  // optimization: {
+  //   runtimeChunk: 'single',
+  // },
   module: {
     rules: [
       {
